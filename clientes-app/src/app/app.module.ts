0cea079a2bdfe8bed4c354b5bdf1fb567df72d6b
Víctor M Gomez <34237832+Victor96Man@users.noreply.github.com>
2019-03-28 11:43:13 +0100

    Cliente Service

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ClientesComponent } from './clientes/clientes.component';
-
+import { ClienteService } from './clientes/cliente.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +18,9 @@ import { ClientesComponent } from './clientes/clientes.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ClienteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
