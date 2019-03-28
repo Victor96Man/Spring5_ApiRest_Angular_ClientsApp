18c60f4eed0ac221584acb19a079675b03f7289e
Víctor M Gomez <34237832+Victor96Man@users.noreply.github.com>
2019-03-28 09:42:13 +0100

    Clientes desde json

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,17 +8,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {id: 1,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
-    {id: 2,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
-    {id: 3,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
-    {id: 4,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
-    {id: 5,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
-    {id: 6,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'}
-  ];
+  clientes: Cliente[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
+  {id: 2,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
+  {id: 3,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
+  {id: 4,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
+  {id: 5,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'},
+  {id: 6,nombre: 'Víctor', apellido: 'Gómez', email: 'victormanuelgomu@gmail.com', createAt: '2019-03-27'}
+];
