48736483d6dbc0341a522b89f87cad808c008c28
Victor96Man <victormanuelgomu@gmail.com>
2019-03-29 16:52:56 +0100

    Metodo borrar cliente

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -17,5 +18,36 @@ export class ClientesComponent implements OnInit {
       clientes => this.clientes = clientes //function anonima
     );
   }
+  delete(cliente: Cliente): void{
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false,
+    })
 
+    swalWithBootstrapButtons.fire({
+      title: '¿Estás Seguro?',
+      text: `¿Seguro de que quieres borrar al Cliente ${cliente.nombre} ${cliente.apellido}?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, borrar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente);
+            swalWithBootstrapButtons.fire(
+              'Borrado!',
+              `El Cliente ${cliente.nombre} ${cliente.apellido} ha sido borrado!`,
+              'success'
+            );
+          }
+        );
+      }
+    });
+  }
 }
